793b34bc8abe26fd18b082228d8b72c529af395e
Tao <hope_14@rediff.com>
2026-03-19 18:02:14 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,10 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-terraform-demo"
   location = "Central India"
 }
+resource "azurerm_storage_account" "storage" {
+  name                     = "tfdemostore12345"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
