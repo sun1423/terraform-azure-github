f310efdb63cdabe977abc3553f184839b4c34bb2
Tao <hope_14@rediff.com>
2026-03-20 17:17:07 +0800

    Create main.tf

---
+++ env/prod/main.tf
@@ -0,0 +1,19 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "testgroup"
+    storage_account_name = "sunstore"
+    container_name       = "suncontainer"
+    key                  = "prod.tfstate"
+
+  }
+}
+
+module "rg" {
+  source   = "../../modules/resource_group"
+  name     = var.rgname
+  location = var.location
+}
