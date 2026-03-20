8a0f2b9e1bc800cf36160841920224f78a111ae7
Tao <hope_14@rediff.com>
2026-03-20 10:20:38 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,3 +8,15 @@ variable "loc" {
   type = string
   default = "Cental India"
 }
+variable "storac" {
+  type = string
+  default "Cental India"
+}
+variable "account_tier" {
+  type = string
+  default "Standrad"
+}
+variable "account_rep_type" {
+  type = string
+  default = "LRS"
+}
