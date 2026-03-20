c872402ee94190a7f715c4dcfb5001dac8098c9f
Tao <hope_14@rediff.com>
2026-03-20 16:10:13 +0800

    Add files via upload

---
+++ modules/resource_group/variables.tf
@@ -0,0 +1,21 @@
+variable "rgn" {
+  description = "rg name"
+  type = string
+}
+
+variable "loc" {
+  type = string
+  default = "Cental India"
+}
+variable "storac" {
+  type = string
+  default = "Cental India"
+}
+variable "account_tier" {
+  type = string
+  default = "Standrad"
+}
+variable "account_rep_type" {
+  type = string
+  default = "LRS"
+}
