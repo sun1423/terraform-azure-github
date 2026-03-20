d35b2356497d8a2844fd40de8f3fa4e1b2b335e4
Tao <hope_14@rediff.com>
2026-03-20 15:46:42 +0800

    Update main.tf

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -8,7 +8,7 @@ terraform {
   }
 }
 module "rg" {
-  source   = "../modules/resource_group"
+  source   = "../../../modules/resource_group"
   name     = var.resource_group_name
   location = var.loc
 }
