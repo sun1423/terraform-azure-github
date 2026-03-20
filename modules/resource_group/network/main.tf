39e4300ab299b9250255b75a415031f9f7f20741
Tao <hope_14@rediff.com>
2026-03-20 18:28:35 +0800

    Update main.tf

--- modules/resource_group/network/main.tf
+++ modules/resource_group/network/main.tf
@@ -4,3 +4,11 @@ resource "azurerm_virtual_network" "vnet" {
   location            = var.location
   resource_group_name = var.rgname
 }
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = var.rgname
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
