fcb7bad4313ecac21e226f377c4b8d6296243d57
Tao <hope_14@rediff.com>
2026-03-20 15:07:56 +0800

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -1,1 +1,14 @@
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "testgroup"
+    storage_account_name = "sunstore"
+    container_name       = "suncontainer"
+    key                  = "prod.tfstate"
+  }
+}
+module "rg" {
+  source   = "../../modules/resource_group"
+  name     = var.rgn
+  location = var.loc
+}
